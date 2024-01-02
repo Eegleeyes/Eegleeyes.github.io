d20749595721f74e124a195b35f835a5836796a7
Eegleeyes <114844231+Eegleeyes@users.noreply.github.com>
2024-01-02 15:58:23 +0000

    added stuff

--- WebGL_Stuff/First/webGLControl.js
+++ WebGL_Stuff/First/webGLControl.js
@@ -1,4 +1,4 @@
-const vertexShaderSource = '#version 300 es \n void main(){gl_Position = vec4(0.0,0.0,0.0,1.0);gl_PointSize = 150.0;}' 
+var vertexShaderSource //= '#version 300 es \n void main(){gl_Position = vec4(0.0,0.0,0.0,1.0);gl_PointSize = 150.0;}' 
 /*= '#version 300 es
 
 void main(){
@@ -7,7 +7,7 @@ void main(){
 }
 '*/
 
-var fragShaderSource = '#version 300 es \n precision mediump float;out vec4 fragColor;void main(){fragColor = vec4(1.0,0.0,0.0,1.0);}'
+var fragShaderSource // = '#version 300 es \n precision mediump float;out vec4 fragColor;void main(){fragColor = vec4(1.0,0.0,0.0,1.0);}'
 /*= '#version 300 es
 
 precision mediump float;
@@ -19,12 +19,17 @@ void main(){
 }
 '*/
 
-/*fetch("frag.txt")
+fetch("vertex.txt")
+  .then((res) => res.text())
+  .then((text) => {
+    vertexShaderSource = text
+   })
+
+fetch("frag.txt")
   .then((res) => res.text())
   .then((text) => {
       fragShaderSource = text
    })
-*/
 
 const canvas = document.getElementById("canvas")
 const gl = canvas.getContext("webgl2")
